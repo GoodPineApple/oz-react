0e15badfbdc1bd4d275bef6515090b6ba6c5a8e7
GoodPineApple <taemin3000@naver.com>
2026-04-02 13:22:57 +0900

    day25 UserForm2

--- src/day25/user-form/UserForm.jsx
+++ src/day25/user-form/UserForm.jsx
@@ -2,9 +2,36 @@ import { useState } from "react";
 
 const UserForm = () => {
   const [name, setName] = useState("");
-  const [age, setAge] = useState("");
+  const [age, setAge] = useState(0);
+  const [user, setUser] = useState({ name: "", age: 0 });
+
   return (
     <div>
+      <input
+        type="text"
+        placeholder="User.Name"
+        value={user.name}
+        onChange={(e) =>
+          setUser({
+            ...user,
+            name: e.target.value,
+          })
+        }
+      />
+      <input
+        type="number"
+        placeholder="User.Age"
+        value={user.age}
+        onChange={(e) =>
+          setUser({
+            ...user,
+            age: e.target.value,
+          })
+        }
+      />
+      <p>
+        Name: {user.name} / Age: {user.age}
+      </p>
       <input
         type="text"
         placeholder="Name"
