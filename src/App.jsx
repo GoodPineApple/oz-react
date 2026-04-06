92140f617088823e22758a7ea9b853dff8a0ea9b
GoodPineApple <taemin3000@naver.com>
2026-04-06 13:36:09 +0900

    day27 route Layout applying

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
 import MainPage from "./main/MainPage";
 import Day25Page from "./day25/Day25Page";
 import Day26Page from "./day26/Day26Page";
@@ -7,9 +8,11 @@ import Day26Page from "./day26/Day26Page";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/day25" element={<Day25Page />} />
-      <Route path="/day26" element={<Day26Page />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/day25" element={<Day25Page />} />
+        <Route path="/day26" element={<Day26Page />} />
+      </Route>
     </Routes>
   );
 }
