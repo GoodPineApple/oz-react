e1e0bf22f8ee273a0868b3db9922053dbf7190a3
GoodPineApple <taemin3000@naver.com>
2026-04-13 14:19:40 +0900

    themeContext useContext function2

--- src/context/MyThemeContext.jsx
+++ src/context/MyThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 // theme color : "light" or "dark"
 const MyThemeContext = createContext("light");
 
-function MyThemeProvider({ children }) {
+const MyThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
@@ -13,6 +13,14 @@ function MyThemeProvider({ children }) {
       {children}
     </MyThemeContext.Provider>
   );
-}
+};
 
 export { MyThemeContext, MyThemeProvider };
+
+// export const useMyTheme = () => {
+//   const context = useContext(MyThemeContext);
+//   if (!context) {
+//     throw new Error("useMyTheme must be used within a MyThemeProvider");
+//   }
+//   return context;
+// };
