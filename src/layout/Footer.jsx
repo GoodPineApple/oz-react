cce0cf2fc50f35525355719155bdedb592a6043b
GoodPineApple <taemin3000@naver.com>
2026-04-13 13:48:04 +0900

    themeContext useContext function

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { MyThemeContext } from "../context/MyThemeContext";
 
 const Footer = () => {
-  const { theme } = useContext(MyThemeContext);
+  const { theme, toggleTheme } = useContext(MyThemeContext);
   return (
     <Box
       component="footer"
@@ -17,6 +18,7 @@ const Footer = () => {
       }}
     >
       <Typography variant="body2">Footer ({theme})</Typography>
+      <Button onClick={toggleTheme}>Toggle Theme</Button>
     </Box>
   );
 };
