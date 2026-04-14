df9b7c9b1d89cf4eee5db4b26d3e47b3eced6c9f
GoodPineApple <taemin3000@naver.com>
2026-04-14 13:09:08 +0900

    zustand log

--- src/store/counterZustandStore.js
+++ src/store/counterZustandStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 export const COUNTER_MAX = 99;
 
-export const useCounterStore = create((set) => ({
+export const useCounterStore = create((set, get) => ({
   value: 0,
   increment: () =>
     set((state) => ({
@@ -12,5 +12,12 @@ export const useCounterStore = create((set) => ({
     set((state) => ({
       value: state.value > 0 ? state.value - 1 : state.value,
     })),
-  reset: () => set({ value: 0 }),
+  reset: () => {
+    console.log("reset");
+    console.log("get() : ", get());
+    console.log("get().value : ", get().value);
+    const value = get().value;
+    if (value === 5) alert("5");
+    set({ value: 0 });
+  },
 }));
